0c6feb177e5f686350f0c23a60d09b510af5e146
Dennis Lord <lorddennis1006@gmail.com>
2018-04-23 20:00:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,3 +7,67 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
   board = [ " ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
+
+def input_to_index(move)
+   index = move.to_i - 1
+   index
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+
+  user_input = gets.strip
+
+  index = input_to_index(user_input)
+  token = current_player(board)
+
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+
+  if index.between?(0,8) && !position_taken?(board, index)
+      puts 'this is a valid move'
+    return true
+  else
+   return false
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter +=1
+  end
+end
+return counter
+end
+
+def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
+
+
+def move(board, index, token)
+  board[index] = token
+  play(board)
+
+end
