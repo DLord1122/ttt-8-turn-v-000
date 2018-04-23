02a2a32a54b04145033f00828927f6cf756cdf07
Dennis Lord <lorddennis1006@gmail.com>
2018-04-23 21:01:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,28 +1,45 @@
 def display_board(board)
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  board = [ " ", " ", " ", " ", " ", " ", " ", " ", " "]
+end
+def turn(board)
+   puts "Please enter 1-9:"
+   user_input = gets.strip
+    index = input_to_index(user_input)
+    token = current_player(board)
+      if valid_move?(board,index)
+     move(board, 1,9)
+      else
+     turn(board)
+   end
+   display_board(board)
+ end
+
+ def position_taken?(board, location)
+  board[location] != " "
+ end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
 end
 def valid_move?(board, index)
-  if (board[index] == " ")
-     true
-  else
-     false
-end
-end
-def position_taken?(board, index)
+   if index.between?(0,8) && position_taken?(board, index)
+      true
+   else
+      false
+    end
+ end
+ def position_taken?(board, index)
   taken = nil
-  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-   taken = false
+  if (board[index] ==  " " || board[index] == nil)
+    taken = false
   else
-  taken = true
+    taken = true
   end
-  taken
-end
-def position_taken?(board, index)
-
- def input_to_index(move)
-   index = move.to_i - 1
+    taken
 end
