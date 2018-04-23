544dc676f06710f2cb0a99d0f5eb485c75f8f251
Dennis Lord <lorddennis1006@gmail.com>
2018-04-23 21:14:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,6 +29,9 @@ def input_to_index(user_input)
 end
 def valid_move?(board, index)
    if index.between?(0,8) && !position_taken?(board, index)
+     true
+   else
+     false 
     end
  end
  def position_taken?(board, index)
