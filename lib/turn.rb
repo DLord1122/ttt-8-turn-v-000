07e95f13732b585ab006562ad2d61f8277e13898
Dennis Lord <lorddennis1006@gmail.com>
2018-04-23 21:07:09 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def input_to_index(user_input)
   index = user_input.to_i - 1
 end
 def valid_move?(board, index)
-   if index.between?(1,9) || position_taken?(board, index)
+   if index.between?(0,8) || position_taken?(board, index)
       true
    else
       false
