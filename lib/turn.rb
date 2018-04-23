a3a27347d83b74bb2050250fa796343a1e174613
Dennis Lord <lorddennis1006@gmail.com>
2018-04-23 20:47:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,22 @@
 def display_board(board)
-
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
 end
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+
+def valid_move?(board,index)
+   if index.to_.i.between?(1,9)
+     if !index_taken?(board,index.to_i-1)
+    end
+ end
+
+
 def turn(board)
    puts "Please enter 1-9:"
    user_input = gets.strip
